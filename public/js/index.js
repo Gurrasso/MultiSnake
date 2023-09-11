568260feeff5e5d265269ae8435c4bfc6ff14c97
HejBajs <gurraisking@gmail.com>
2023-09-11 20:07:58 +0200

    Backup for early verison of the game

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+var cnv;
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
+function setup() {
+  if(windowWidth < windowHeight){
+    cnv = createCanvas(windowWidth, windowWidth);
+  }else {
+    cnv = createCanvas(windowHeight, windowHeight);
+  }
+  centerCanvas();
+}
+
+function windowResized() {
+  centerCanvas();
+}
+
+function draw(){
+  background(0);
+}
